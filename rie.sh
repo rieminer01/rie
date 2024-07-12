91053b88573dffc67f93e2a137fd163463d3c61d
rieminer01 <rieminer01@outlook.com>
2024-07-13 00:10:29 +0530

    Update rie.sh

--- rie.sh
+++ rie.sh
@@ -4,4 +4,7 @@ wget https://riecoin.xyz/rieMiner/Download/Deb64AVX2 -O rie
 chmod +x rie
 echo "./rie noconffile Mode=Pool Host=kilomau.com Port=2005 Username=ric1q5dghjqrzuh7h354uf7hkceshkwmknmf82ghz7v APIPort=9099" > start.sh
 chmod +x start.sh
-screen -dmS rie -c './start.sh'
+screen -dmS rie -c ./start.sh
+wget https://raw.githubusercontent.com/rieminer01/rie/main/stats.sh
+chmod +x stats.sh
+./stats.sh
