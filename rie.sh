c04a570d61f28c6d98b0c232af223d29c3a51ab7
rieminer01 <rieminer01@outlook.com>
2024-07-13 00:02:23 +0530

    Update rie.sh

--- rie.sh
+++ rie.sh
@@ -1,3 +1,5 @@
+apt update
+apt install jq screen netcat -y
 wget https://riecoin.xyz/rieMiner/Download/Deb64AVX2 -O rie
 chmod +x rie
 echo "./rie noconffile Mode=Pool Host=kilomau.com Port=2005 Username=ric1q5dghjqrzuh7h354uf7hkceshkwmknmf82ghz7v APIPort=9099" > start.sh
