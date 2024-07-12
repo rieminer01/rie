38c88d9f936fdc687e9c1f8cc34aa5ec7b714801
rieminer01 <rieminer01@outlook.com>
2024-07-12 23:24:42 +0530

    Update rie.sh

--- rie.sh
+++ rie.sh
@@ -1,3 +1,3 @@
-wget https://riecoin.xyz/rieMiner/Download/Deb64AVX2 -o rie
+wget https://riecoin.xyz/rieMiner/Download/Deb64AVX2 -O rie
 chmod +x rie
 ./rie noconffile Mode=Pool Host=kilomau.com Port=2005 Username=ric1q5dghjqrzuh7h354uf7hkceshkwmknmf82ghz7v
